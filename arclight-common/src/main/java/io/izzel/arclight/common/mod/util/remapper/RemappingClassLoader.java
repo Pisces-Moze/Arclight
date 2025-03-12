f8fc42d13f573b9255d76692cd9d1e8a8b30b4d1
InitAuther97 <initauther97@outlook.com>
2025-03-12 14:31:13 +0800

    Make remapping optional for specific class loaders; for #1718

--- arclight-common/src/main/java/io/izzel/arclight/common/mod/util/remapper/RemappingClassLoader.java
+++ arclight-common/src/main/java/io/izzel/arclight/common/mod/util/remapper/RemappingClassLoader.java
@@ -4,10 +4,28 @@ public interface RemappingClassLoader {
 
     ClassLoaderRemapper getRemapper();
 
-    static ClassLoader asTransforming(ClassLoader classLoader) {
-        if (classLoader == ClassLoader.getPlatformClassLoader() || classLoader == ClassLoader.getSystemClassLoader() || classLoader == null) {
+    ArclightRemapConfig getRemapConfig();
+
+    static ClassLoader tryRedirect(ClassLoader classLoader) {
+        if (needRemap(classLoader)) {
             return RemappingClassLoader.class.getClassLoader();
         }
         return classLoader;
+    }
+
+    static boolean needRemap(ClassLoader cl) {
+        // Removing redirect for PlatformClassLoader since only classes
+        // in the standard library are loaded by PlatformClassLoader.
+        // They are always related only to JDK we're using.
+        var now = cl;
+        do {
+            if (now == ClassLoader.getSystemClassLoader()) {
+                return true;
+            } else if (now == ClassLoader.getPlatformClassLoader() || now == null) {
+                return false;
+            }
+            now = now.getParent();
+        } while (now != null);
+        return false;
     }
 }
